34ef3c1a9d5020898243ae34b9bb86971a629b36
PatrickOvi <dinokillersyt@gmail.com>
2024-07-22 00:41:46 -0400

    avances evaluacion 4
    idea en mente con un poco de desarrollo de algunas paginas

---
+++ src/pages/Pagina1.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import Form  from 'react-bootstrap/Form'
+
+
+import { Button } from 'react-bootstrap';
+
+export const Pagina1 = ()=>{
+    const [nombre, setNombre] = useState("Joselito")
+    const [apellido, setApellido] = useState("Joselito")
+    const [errorNombre, setErrorNombre] = useState("Joselito")
+
+    const validarNombre = (valor:string)=>{
+        if(valor.length>4){
+            setErrorNombre("");
+        }else{
+            setErrorNombre("Debes ingresar 4 caracteres como minimo")
+        }
+        setNombre(valor);
+
+    }
+
+    const handleRegistrar = ()=>{
+        console.log("Se registro con exito");
+        alert("Listo!"+nombre+" "+apellido);
+    }
+    
+    
+    return(
+        <>
+        <h1>Crear Usuario</h1>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicNombre">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type='text' placeholder='ingrese un nombre' onChange={(e)=>validarNombre(e.currentTarget.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicApellido">
+                <Form.Label>Apellido</Form.Label>
+                <Form.Control type='text' placeholder='ingrese un apellido' onChange={(e)=>setApellido(e.currentTarget.value)}/>
+            </Form.Group>
+            <Button variant='primary' type='button' onClick={handleRegistrar}>Registrar</Button>
+        </Form>
+
+
+
+
+
+
+        <form>
+            <input type='text' id='nombre' name='nombre'/>
+            <input type='text' id='apellido' name='apellido'/>
+            <button>Registrar</button>
+        </form>
+        </>
+    )
+}
+export default Pagina1
---
+++ src/pages/Pagina2.tsx
@@ -0,0 +1,11 @@
+export const Pagina2 = ()=>{
+    return( 
+    <div>
+        <button className="pagina2">Crear Usuario</button>
+        <button className="pagina2">Control Empleados</button>
+        <button className="pagina2">Visualizar Datos</button>
+        <button className="pagina2">Salir</button>
+    </div>
+    )
+}
+export default Pagina2
